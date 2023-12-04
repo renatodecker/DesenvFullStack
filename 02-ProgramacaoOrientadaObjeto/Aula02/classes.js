94aac1c45dc7e32939ec19fb228e8d4eba7fa207
Renato Deccker <67979866+renatodecker@users.noreply.github.com>
2023-12-04 16:46:17 -0300

    Update classes.js

--- 02-ProgramacaoOrientadaObjeto/Aula02/classes.js
+++ 02-ProgramacaoOrientadaObjeto/Aula02/classes.js
@@ -6,7 +6,7 @@ class Pessoa{
     };
 
     ola(){
-        console.log('Olá');
+        console.log('Olá, eu sou o ' + this.nome);
     };
     calculaIdade(){
         return new Date().getFullYear() - this.anoNascimento;
@@ -17,3 +17,31 @@ const pessoa = new Pessoa(['Renato','Decker'],1983,'Analista de Sistemas');
 console.log(pessoa);
 pessoa.ola();
 console.log(pessoa.calculaIdade());
+
+class Estudante extends Pessoa{
+    constructor(nome,anoNascimento,matricula){
+        super(nome,anoNascimento,'estudante');
+        this.matricula = matricula;
+    }
+    escreveMatricula(){
+        return this.matricula;
+    }
+    ola(){
+        super.ola();
+        console.log('Colega');
+    }
+}
+
+const aluno = new Estudante('Renato',1983,'Estudante','01238400');
+console.log(aluno.escreveMatricula());
+console.log(aluno.ola());
+
+class Professor extends Pessoa{
+    constructor(nome,anoNascimento,titulacao){
+        super(nome,anoNascimento,'Professor');
+        this.titulacao = titulacao;
+    }
+    getTitulacao(){
+        return this.titulacao;
+    }
+}
