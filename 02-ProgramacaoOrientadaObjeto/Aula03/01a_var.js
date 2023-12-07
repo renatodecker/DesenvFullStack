c3182a1989954b678a50998e58b9c1e5d45316ef
Renato Deccker <67979866+renatodecker@users.noreply.github.com>
2023-12-07 13:17:21 -0300

    Disc02-Aula03-Parte01

---
+++ 02-ProgramacaoOrientadaObjeto/Aula03/01a_var.js
@@ -0,0 +1,12 @@
+function testvar(){
+    var var01 = 'var 01'
+    {
+        var var02 = 'var 02';
+        console.log('01a.' + var01)
+        console.log('01b.' + var02)
+    }
+    console.log('02a.' + var01)
+    console.log('02b.' + var02)
+}
+
+testvar();
---
+++ 02-ProgramacaoOrientadaObjeto/Aula03/01c_var.js
@@ -0,0 +1,16 @@
+//01c_var.js
+function testVar(){
+    var var01="var 01"
+
+    {
+        console.log("01a."+var01)
+
+        var var01="var 02"
+        console.log("02a."+var01)
+    }
+
+    var var01="var 03"
+    
+    console.log("03a."+var01)
+}
+testVar()
